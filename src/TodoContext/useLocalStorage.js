df4dbd184cee8a0f50ab6cebf660add4a488803d
Stiven Trujillo <strujillodev@gmail.com>
2022-01-02 21:49:45 -0500

    Fix useEffect deps

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -5,27 +5,32 @@ const useLocalStorage = (itemName, initialValue) => {
     const [loading, setLoading] = React.useState(true);
     const [item, setItem] = React.useState(initialValue);
 
-    // simular que se tarda un rato
-    React.useEffect(() => {
-        console.log('me ejecute')
-        setTimeout(() => {
-            try {
-                const localStorageItem = localStorage.getItem(itemName);
-                let parsedItem = initialValue;
+    const getItemFromLocalStorage = () => {
+        try {
+            const localStorageItem = localStorage.getItem(itemName);
+            let parsedItem = initialValue;
 
-                if (!localStorageItem) {
-                    localStorage.setItem(itemName, JSON.stringify(initialValue));
-                } else {
-                    parsedItem = JSON.parse(localStorageItem);
-                }
-
-                setItem(parsedItem);
-                setLoading(false);
-            } catch (error) {
-                // setError(true); o mejor:
-                setError(error);
+            if (!localStorageItem) {
+                localStorage.setItem(itemName, JSON.stringify(initialValue));
+            } else {
+                parsedItem = JSON.parse(localStorageItem);
             }
+
+            setItem(parsedItem);
+            setLoading(false);
+        } catch (error) {
+            // setError(true); o mejor:
+            setError(error);
+        }
+    }
+
+    React.useEffect(() => {
+        console.log('useLocalStorage useEffect executed')
+        // simular que se tarda un rato
+        setTimeout(() => {
+            getItemFromLocalStorage()
         }, 3000);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const saveItem = (newItem) => {
